4c8ee9ad0bf9087593e03285aef7a35d0b54d4d1
“Aamir <aamirhussainazad786@gmail.com>
2025-04-09 17:35:31 +0530

    commit add

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div >
-      <h1>Hello</h1>
+      <h1>Hello Aamir</h1>
     </div>
   </React.StrictMode>
 );
-
-
--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,14 +1,8 @@
-// Correct
-import ReactDOM from 'react-dom';
-// Or for React 18+
-import { createRoot } from 'react-dom/client';
-
-
 import React from 'react';
 import Hero from './Hero';
 import Awards from './Awards';
 import Stats from './Stats'; // Update this based on the correct location
-import Pricing from './Pricing'; 
+import Pricing from './pricing'; 
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
 import Navbar from '../Navbar';
